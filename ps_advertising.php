2a53f54bcc27d207370c293d88d7b97f647a18b6
MathiasReker <mathias@reker.dk>
2018-12-29 23:57:40 +0100

    Cast spaces

--- ps_advertising.php
+++ ps_advertising.php
@@ -165,9 +165,9 @@ class Ps_Advertising extends Module implements WidgetInterface
                     // Set the image name with a name contextual to the shop context
                     $this->adv_imgname = 'advertising';
                     if (Shop::getContext() == Shop::CONTEXT_GROUP) {
-                        $this->adv_imgname = 'advertising-g'.(int)$this->context->shop->getContextShopGroupID();
+                        $this->adv_imgname = 'advertising-g'.(int) $this->context->shop->getContextShopGroupID();
                     } elseif (Shop::getContext() == Shop::CONTEXT_SHOP) {
-                        $this->adv_imgname = 'advertising-s'.(int)$this->context->shop->getContextShopID();
+                        $this->adv_imgname = 'advertising-s'.(int) $this->context->shop->getContextShopID();
                     }
 
                     // Copy the image in the module directory with its new name
@@ -299,7 +299,7 @@ class Ps_Advertising extends Module implements WidgetInterface
         $helper = new HelperForm();
         $helper->show_toolbar = false;
         $helper->table = $this->table;
-        $lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
+        $lang = new Language((int) Configuration::get('PS_LANG_DEFAULT'));
         $helper->default_form_language = $lang->id;
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
         $this->fields_form = array();
